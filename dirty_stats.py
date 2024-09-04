6f1982d4fc040b0c140515a4a295188b7c7b2107
aiden5687 <98786979+aiden5687@users.noreply.github.com>
2024-09-04 10:28:52 -0400

    finished dirty_stats.py

--- dirty_stats.py
+++ dirty_stats.py
@@ -5,17 +5,31 @@ def count_missing_values(db_path):
     cursor = conn.cursor()
     cursor.execute(f"PRAGMA table_info(events);")
     columns = cursor.fetchall()
+    count =0
     for column in columns:
+        count+=1
         column_name = column[1]
-        # Count NULL values in the column
+        quoted_column_name = f'"{column_name}"'
+
         cursor.execute(f"""
             SELECT COUNT(*) 
             FROM events
-            WHERE {column_name} IS NULL;
+            WHERE {quoted_column_name} IS NULL OR {quoted_column_name} = '';
         """)
         missing_count = cursor.fetchone()[0]
         print(f"Column: {column_name}, Missing Count: {missing_count}")
+        
+        if (count >1 and count <5) or count ==6:
+            cursor.execute(f"""
+            SELECT COUNT(DISTINCT {quoted_column_name}) 
+            FROM events;
+            """)
+            unique_count = cursor.fetchone()[0]
+            print(f"Column: {column_name}, Unique Count: {unique_count}")
     conn.close()
+
+
+#count_missing_values('health_events_data.db')
 
 
 count_missing_values('health_events_data.db')
